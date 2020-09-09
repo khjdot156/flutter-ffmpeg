81bcd5fc1446f7bfb838c408094ccd06f2d5c6d8
khjdot156 <ngoctam.vinhcity@gmail.com>
2020-09-09 09:07:37 +0700

    Update FlutterFfmpegPlugin.m

--- ios/Classes/FlutterFfmpegPlugin.m
+++ ios/Classes/FlutterFfmpegPlugin.m
@@ -306,7 +306,7 @@ static NSString *const EVENT_STAT = @"FlutterFFmpegStatisticsCallback";
         }
 
         NSDictionary *metadataDictionary = [mediaInformation getMetadataEntries];
-        NSLog(@"FFprobe exited with rc: %d\n", metadataDictionary);
+        NSLog(@"=>>>>>>>metadataDictionary %@.\n", metadataDictionary);
         if (metadataDictionary != nil && ([metadataDictionary count] > 0)) {
             dictionary[@"metadata"] = metadataDictionary;
         }
